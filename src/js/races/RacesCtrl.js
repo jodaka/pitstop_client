1b76439d799c795b959134a0469f77bd5d660af3
Anton Kudris <anton@kudris.com>
2016-03-13 13:37:56 +0300

    moar fixes

--- src/js/races/RacesCtrl.js
+++ src/js/races/RacesCtrl.js
@@ -13,12 +13,27 @@ function RaceCtrlFactory( AppConfig, $scope, $routeParams, $location ) {
             }
         }
 
-        $scope.periodIsActive = function( period ) {
+        $scope.setPeriod = function ( period ) {
 
+            if ( period === 'all' ) {
+                $scope.period = period;
+            } else {
+                $scope.period = 'date';
+                $scope.date = period;
+            }
+
+            saveUrlParams();
+        };
+
+        $scope.periodIsActive = function ( period ) {
             if ( $scope.period === 'all' && period === 'all' ) {
                 return true;
             }
+            if ( $scope.period === 'date' && period === $scope.date ) {
+                return true;
+            }
 
+            return false;
         };
 
         $scope.selectClub = function ( id ) {
@@ -47,7 +62,7 @@ function RaceCtrlFactory( AppConfig, $scope, $routeParams, $location ) {
                 clubs.push( $scope.clubsIds[ $scope.selectedClubs[ i ] ] );
             }
 
-            var period = ( date === null ) ? $scope.page : $scope.date;
+            var period = ( $scope.date === null ) ? $scope.page : $scope.date;
             $location.path( '/races/' + clubs.join( ',' ) + '/' + $scope.period + '/' + period );
         };
 
@@ -72,9 +87,14 @@ function RaceCtrlFactory( AppConfig, $scope, $routeParams, $location ) {
         var page = $routeParams.page;
         var date = null;
 
-        if ( period !== 'all' || period !== 'date' ) {
+        if ( !( period === 'all' || period === 'date' ) ) {
             period = 'all';
         }
+
+        var today = new Date();
+        $scope.today = today.toISOString().slice( 0, 10 );
+        today.setDate( today.getDate() - 1 );
+        $scope.yesterday = new Date( today.getTime() ).toISOString().slice( 0, 10 );
 
         // check if page is actually a day
         if ( period === 'date' && /\d\d\d\d-\d\d-\d\d/.test( page ) ) {
@@ -112,10 +132,10 @@ function RaceCtrlFactory( AppConfig, $scope, $routeParams, $location ) {
         clubsParam = clubsParam.split( ',' );
         for ( var i = 0; i < clubsParam.length; i++ ) {
             if ( AppConfig.clubsEn.hasOwnProperty( clubsParam[ i ] ) ) {
-                $scope.selectedClubs.push( String(AppConfig.clubsEn[ clubsParam[ i ] ]) );
+                $scope.selectedClubs.push( String( AppConfig.clubsEn[ clubsParam[ i ] ] ) );
             }
         }
 
-        console.log(5555, $scope.selectedClubs);
+        console.log( 5555, $scope.selectedClubs );
 
 } ] );
