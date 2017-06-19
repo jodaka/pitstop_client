d4d6531d8cddcac00ab2f2ea1193c30caff631bb
Anton Kudris <anton@kudris.com>
2017-06-19 15:29:38 +0300

    added babel

--- dev/gulp/tasks/war.js
+++ dev/gulp/tasks/war.js
@@ -1,85 +1,76 @@
-/*global require*/
-let gulp = require( 'gulp' );
-let uglify = require( 'gulp-uglify' );
-let config = require( '../config' );
-let del = require( 'del' );
-let runSequence = require( 'run-sequence' );
-let moreLess = require( 'gulp-more-less' );
-let replace = require( 'gulp-replace' );
+const gulp = require('gulp');
+const uglify = require('gulp-uglify');
+const config = require('../config');
+const del = require('del');
+const runSequence = require('run-sequence');
+const moreLess = require('gulp-more-less');
+const replace = require('gulp-replace');
+const babel = require('gulp-babel');
 
-let handleErrors = ( err ) => {
-    console.error( err );
+const handleErrors = (err) => {
+    console.error(err);
 };
 
-gulp.task( 'build:copy-app', [ 'copy' ], () => {
+gulp.task('build:copy-app', ['copy'], () =>
+    gulp.src(`${config.paths.www_site}/**/*`, {
+        base: './app/'
+    })
+    .pipe(gulp.dest(config.paths.peace))
+);
 
-    return gulp.src( config.paths.www_site + '/**/*', {
-            base: './app/'
-        } )
-        .pipe( gulp.dest( config.paths.peace ) );
+gulp.task('build:includesVersion', () => {
+    const ver = Date.now();
 
-} );
-
-gulp.task( 'build:includesVersion', () => {
-
-    let ver = Date.now();
-
-    gulp.src( [ config.paths.peace + '/index.html' ] )
-        .pipe( replace( /_version_/g, 'ver=' + ver ) )
-        .pipe( gulp.dest( config.paths.peace ) );
-} );
+    gulp.src([`${config.paths.peace}/index.html`])
+        .pipe(replace(/_version_/g, `ver=${ver}`))
+        .pipe(gulp.dest(config.paths.peace));
+});
 
 
-gulp.task( 'build:turnOffDebug', () => {
+gulp.task('build:turnOffDebug', () => {
+    gulp.src([`${config.paths.peace}/js/main.js`])
+        .pipe(replace(/(.DEBUG.)\s*:\s*true/, '$1:false'))
+        .pipe(gulp.dest(`${config.paths.peace}/js/`));
+});
 
-    gulp.src( [ config.paths.peace + '/js/main.js' ] )
-        .pipe( replace( /(.DEBUG.)\s*:\s*true/, '$1:false' ) )
-        .pipe( gulp.dest( config.paths.peace + '/js/' ) );
-
-} );
-
-gulp.task( 'build:clean-app', () => {
-    return del( [ config.paths.www_site ] );
-} );
+gulp.task('build:clean-app', () => del([config.paths.www_site]));
 
 
-gulp.task( 'build:copy', ( cb ) => {
-
+gulp.task('build:copy', (cb) => {
     runSequence(
         'build:clean-app',
         'build:copy-app',
         'build:turnOffDebug',
         'build:includesVersion',
-        cb );
+        cb);
+});
 
-} );
-
-gulp.task( 'build:minify-js', [ 'build:copy' ], () => {
-
-    let paths = [
-        config.paths.peace + '/js/*.js',
+gulp.task('build:minify-js', ['build:copy'], () => {
+    const paths = [
+        `${config.paths.peace}/js/*.js`
     ];
 
-    return gulp.src( paths )
-        .pipe( uglify() ).on( 'error', handleErrors )
-        .pipe( gulp.dest( config.paths.peace + '/js/' ) );
+    return gulp.src(paths)
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(uglify()).on('error', handleErrors)
+        .pipe(gulp.dest(`${config.paths.peace}/js/`));
+});
 
-} );
-
-gulp.task( 'build:minify-css', [ 'build:copy' ], () => {
-
-    let paths = [
-        config.paths.peace + '/css/*.css',
+gulp.task('build:minify-css', ['build:copy'], () => {
+    const paths = [
+        `${config.paths.peace}/css/*.css`
     ];
 
-    return gulp.src( paths )
-        .pipe( moreLess( {
-            'more': true,
-            'less': false
-        } ) )
-        .pipe( gulp.dest( config.paths.peace + '/css/' ) );
-} );
+    return gulp.src(paths)
+        .pipe(moreLess({
+            more: true,
+            less: false
+        }))
+        .pipe(gulp.dest(`${config.paths.peace}/css/`));
+});
 
-gulp.task( 'build:minify', [ 'build:minify-css', 'build:minify-js' ] );
+gulp.task('build:minify', ['build:minify-css', 'build:minify-js']);
 
-gulp.task( 'build', [ 'build:minify' ] );
+gulp.task('build', ['build:minify']);
